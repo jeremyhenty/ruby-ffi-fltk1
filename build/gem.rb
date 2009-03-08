3cf238ac73dfb64a80baea93900e8107fedddf2e
Jeremy Henty <onepoint@starurchin.org>
2009-03-08 22:17:51 +0000

    Demonstration: Adjusters.

--- build/gem.rb
+++ build/gem.rb
@@ -35,7 +35,7 @@ module Build
     s.files =
       FileList["COPYING", "AUTHORS",
                "lib/**/*.rb", "build/*.rb", "erb/**/*",
-               "bin/*", "demo/*"].to_a
+               "bin/*", "demo/**/*"].to_a
     s.extensions = "Rakefile"
     s.executables = [ DEMO_NAME ]
   end
